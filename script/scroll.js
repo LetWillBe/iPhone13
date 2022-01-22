11babcd10f39864e57d64f75fddbbbcc93f5177e
LetWillBe <balksamuri@gmail.com>
2022-01-22 15:23:29 +0200

    Lesson2

--- script/scroll.js
+++ script/scroll.js
@@ -1,61 +1,69 @@
-/*получаем псевдомассив со ссылками из нав-меню*/
-const links = document.querySelectorAll('.header-menu__item a');
+const scrollFunc = () => {
+    /*получаем псевдомассив со ссылками из нав-меню*/
+    const links = document.querySelectorAll('.header-menu__item a');
 
-/*Вызов полифила для поддержки кроссбарузерности(Сафари)*/
-seamless.polyfill();
+    /*ДЗ1 - ссылка на хар-ки*/
+    const characteristics = document.querySelector('.card-details__link-characteristics');
 
-/*Перебираем псевдомассив*/
-links.forEach((element) => {
-    /*вешаем обработчик события на клик по ссылке в нав-меню*/
-    element.addEventListener('click', (event) => {
-        /*отключаем действие по умолчанию на клик*/
-        event.preventDefault();
-        /*получаем id*/
-        const id = element.getAttribute('href').substring(1);
-        /*по полученному id получаем целевую область документа*/
-        const section = document.getElementById(id);
-        /*Проверка на наличие целевой области (id !== '#')*/
-        if (section) {
-            /*Скролл с помощью scrollIntoView (не работает в Сафари)*/
-            section.scrollIntoView({
-                behavior: 'smooth',
-                block: 'start'
-            });
-        } else {
-            /*Скролл с помощью полифила*/
-            seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
-                behavior: "smooth",
-                block: "center",
-                inline: "center",
-            });
-        }
+    //ДЗ1- Массив со ссылками из навигации и ссылка на хар-ки
+    const newarray = [...links, characteristics];
+
+    /*Вызов полифила для поддержки кроссбарузерности(Сафари)*/
+    seamless.polyfill();
+
+    /*Перебираем псевдомассив*/
+    newarray.forEach((element) => {
+        /*вешаем обработчик события на клик по ссылке в нав-меню*/
+        element.addEventListener('click', (event) => {
+            /*отключаем действие по умолчанию на клик*/
+            event.preventDefault();
+            /*получаем id*/
+            const id = element.getAttribute('href').substring(1);
+            /*по полученному id получаем целевую область документа*/
+            const section = document.getElementById(id);
+            /*Проверка на наличие целевой области (id !== '#')*/
+            if (section) {
+                /*Скролл с помощью scrollIntoView (не работает в Сафари)*/
+                section.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start'
+                });
+            } else {
+                /*Скролл с помощью полифила*/
+                seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
+                    behavior: "smooth",
+                    block: "center",
+                    inline: "center",
+                });
+            }
+        });
     });
-});
+};
 
-/*ДЗ*/
+scrollFunc();
 
+/*ДЗ1*/
 /*получаем область документа по селектору*/
-const characteristics = document.querySelector('.card-details__link-characteristics');
-
+//const characteristics = document.querySelector('.card-details__link-characteristics');
 /*вешаем на область обработчик события по клику*/
-characteristics.addEventListener('click', (event) => {
-    /*отключаем действие по умолчанию на событие*/
-    event.preventDefault();
-    /*получаем id целевой области*/
-    const id = characteristics.getAttribute('href').substring(1);
-    /*получаем саму целевую область из документа*/
-    const target = document.getElementById(id);
-    /*скролл к целевой области с помощью полифила и проверкой на id !== '#'(мало ли что)*/
-    if (target) {
-        seamless.elementScrollIntoView(target, {
-            behavior: 'smooth',
-            block: 'start'
-        });
-    } else {
-        seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
-            behavior: "smooth",
-            block: "center",
-            inline: "center",
-        });
-    }
-});
+//characteristics.addEventListener('click', (event) => {
+/*отключаем действие по умолчанию на событие*/
+//event.preventDefault();
+/*получаем id целевой области*/
+//const id = characteristics.getAttribute('href').substring(1);
+/*получаем саму целевую область из документа*/
+//const target = document.getElementById(id);
+/*скролл к целевой области с помощью полифила и проверкой на id !== '#'(мало ли что)*/
+//    if (target) {
+//        seamless.elementScrollIntoView(target, {
+//            behavior: 'smooth',
+//            block: 'start'
+//        });
+//    } else {
+//        seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
+//            behavior: "smooth",
+//            block: "center",
+//            inline: "center",
+//        });
+//    }
+//});
---
+++ script/tab.js
@@ -0,0 +1,66 @@
+const tabsFunc = () => {
+    // Поля с характеристиками (табы)
+    const tabs = document.querySelectorAll('.card-detail__change');
+    //Заголовок табов
+    const tabsTitle = document.querySelector('.card-details__title');
+    //Цена
+    const tabsPrice = document.querySelector('.card-details__price');
+    //картинка
+    const tabsImage = document.querySelector('.card__image_item');
+    //тектовый конент табов
+    const tabsOptions = [{
+            name: 'Graphite',
+            memory: '128',
+            price: '59900',
+            image: 'img/iPhone-graphite.webp'
+        },
+        {
+            name: 'Silver',
+            memory: '256',
+            price: '79990',
+            image: 'img/iPhone-silver.webp'
+        },
+        {
+            name: 'Sierra Blue',
+            memory: '512',
+            price: '99999',
+            image: 'img/iPhone-sierra_blue.webp'
+        }
+    ];
+
+    //ф-ция смены текстового контента таба
+    //index - идентификатор таба
+    const changeContent = (index) => {
+        tabsTitle.textContent = `Смартфон Apple iPhone 13 Pro ${tabsOptions[index].memory}GB ${tabsOptions[index].name}`;
+        tabsPrice.textContent = `${tabsOptions[index].price}₽`;
+        tabsImage.setAttribute('src', tabsOptions[index].image);
+    };
+
+    //функция на смену активного таба
+    const changeActiveTabs = (indexClickedTab) => {
+        tabs.forEach((tab, index) => {
+            //убираем class = 'active' у всех табов
+            tab.classList.remove('active');
+            //добавляем active элементу по которому будет сделан click
+            if (index == indexClickedTab) {
+                tab.classList.add('active');
+            }
+        });
+        //смена текста в заголовке у таба
+        changeContent(indexClickedTab);
+    };
+
+    //перебор массива с табами
+    tabs.forEach((tab, index) => {
+        //обработчик события на клик по табу
+        tab.addEventListener('click', () => {
+            //добавляем табу active по событию click
+            changeActiveTabs(index);
+        });
+    });
+
+    //По умолчанию в табах выводится информация о первой модели, вместо той что в верстке
+    changeContent(0);
+};
+
+tabsFunc();
