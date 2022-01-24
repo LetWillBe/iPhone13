9265b819710c9012d08db39cae5a6061f64a1c6e
LetWillBe <balksamuri@gmail.com>
2022-01-24 18:29:19 +0200

    Homework 4

--- script/getData.js
+++ script/getData.js
@@ -1,20 +1,72 @@
 const getData = () => {
-    fetch('https://jsonplaceholder.typicode.com/todos')
-        .then((response) => {
-            if (response.ok) {
-                return response.json();
-            } else {
-                throw new Error('Данные получены с ошибкой!');
-            }
-        })
-        .then((data) => {
-            //console.log(data);
-        })
-        .catch((error) => {
-            //console.error(error.message);
-        })
-        .finally(() => {
-            //console.log('finally');
+    //получаем карточки товара
+    const list = document.querySelector('.cross-sell__list');
+    //кнопка
+    const btn = document.querySelector('.cross-sell__add');
+    //количество карточек товара в группе
+    let stack = 4;
+    //счётчик групп товаров
+    let count = 1;
+
+    const render = (data) => {
+        //очищаем карточки товара
+        list.innerHTML = '';
+        //отрисовка карточек товара
+        data.forEach(item => {
+            list.insertAdjacentHTML('beforeend', `
+                <li>
+                    <article class="cross-sell__item">
+                        <img class="cross-sell__image" src="./${item.photo}" alt="${item.id}">
+                        <h3 class="cross-sell__title">${item.name}</h3>
+                        <p class="cross-sell__price">${item.price}₽</p>
+                        <button type="button" class="button button_buy cross-sell__button">Купить</button>
+                    </article>
+                </li>
+                    `);
         });
+    };
+
+    //выбираем из массива data нужное количество товаров
+    const sliceArray = (data, index) => {
+        return data.slice(0, index);
+    };
+    //делим массив data на группы
+    const changeData = (data) => {
+        //определяем размер стека показываемых товаров
+        const newStack = stack * count;
+        //массив со стеком товаров
+        render(sliceArray(data, newStack));
+        //проверка: если в data ещё остаются товары - кнопка не убирается
+        if (data.length > newStack) {
+            count++;
+        } else {
+            //убираем кнопку
+            btn.style.display = 'none';
+        }
+    };
+
+    const getGoods = () => {
+        //получаем данные из json
+        fetch('../cross-sell-dbase/dbase.json')
+            //обработка запроса
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error('Данные получены с ошибкой!');
+                }
+            })
+            //отправка массива data в функцию changeData
+            .then((data) => {
+                changeData(data);
+            })
+            .catch((error) => {
+                console.error(error.message);
+            });
+    };
+    //обработчик события на кнопку "Показать ещё"
+    btn.addEventListener('click', getGoods);
+
+    getGoods();
 };
 getData();
---
+++ script/sendFrom.js
@@ -0,0 +1,56 @@
+const sendFrom = () => {
+    const btnOpenModal = document.querySelector('.card-details__button_delivery');
+    const modal = document.querySelector('.modal');
+    const cardDetailsTitle = document.querySelector('.card-details__title');
+    const modalTitle = modal.querySelector('.modal__title');
+    const modalClose = modal.querySelector('.modal__close');
+    const modalForm = modal.querySelector('form');
+
+    //открытие модального окна
+    btnOpenModal.addEventListener('click', () => {
+        //делаем видимым модальное окно
+        modal.style.display = 'flex';
+        //меняем заголовок модального окна
+        modalTitle.textContent = cardDetailsTitle.textContent;
+    });
+    //кнопка закрытия модального окна
+    modalClose.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+    //обработчик события на кнопку отправки формы
+    modalForm.addEventListener('submit', (event) => {
+        //очищаем действие по умолчанию
+        event.preventDefault();
+        //находим три поля формы
+        const labels = modal.querySelectorAll('.modal_label');
+
+        const sendMesage = {};
+
+        //заполняем три поля формы (Куда, кому, телефон)
+        labels.forEach(label => {
+            const span = label.querySelector('span');
+            const input = label.querySelector('input');
+            sendMesage[span.textContent] = input.value;
+            console.log(span.textContent);
+            console.log(input.value);
+        });
+        //POST-запрос
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+                method: 'POST',
+                body: JSON.stringify(sendMesage),
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                },
+            })
+            .then(() => {
+                console.log('Отправлено!');
+            });
+
+        //Очистка формы и закрытие окна
+        document.addEventListener('submit', (event) => {
+            event.target.reset();
+            modal.style.display = 'none';
+        });
+    });
+};
+sendFrom();
